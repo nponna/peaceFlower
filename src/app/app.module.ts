485239f6960c4718c02c2d9579e556efa6206cf9
Nikitha Ponna <nikitha.ponna@sap.com>
2017-08-14 09:58:01 -0700

    Add all files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'reviews', component: ReviewsComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavigationComponent,
+    ReviewsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
