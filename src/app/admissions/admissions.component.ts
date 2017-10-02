14962b1c68ab8d18dfc0d8e64fb24de8e998b7c9
Nikitha Ponna <nikithaponna102@gmail.com>
2017-10-01 20:17:03 -0700

    Final admissions form

--- src/app/admissions/admissions.component.ts
+++ src/app/admissions/admissions.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, SecurityContext, ViewChild, ElementRef } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
 import { missionStatement, whyPeaceFlower } from '../data/home';
 
 import { AdmissionsService } from '../services/admissions.service';
@@ -11,20 +10,13 @@ import { AdmissionsService } from '../services/admissions.service';
   styleUrls: ['./admissions.component.css']
 })
 export class AdmissionsComponent implements OnInit{
-  @ViewChild('cry') container: ElementRef;
   public missionStatementDisplayed: string;
   public whyPeaceFlowerDisplayed: string;
-  public scriptNeeded: any;
+  public message: string;
+  public errorMessage: string;
 
-  constructor(private admissionsService:AdmissionsService, private sanitizer: DomSanitizer) {
-    this.scriptNeeded = sanitizer.bypassSecurityTrustHtml('<script data-cfasync="false" type="text/javascript" src="https://cdn.rawgit.com/dwyl/html-form-send-email-via-google-script-without-server/master/form-submission-handler.js"></script>');
-    
-    //let scriptNeeded = sanitizer.bypassSecurityTrustScript('<script data-cfasync="false" type="text/javascript" src="https://cdn.rawgit.com/dwyl/html-form-send-email-via-google-script-without-server/master/form-submission-handler.js"></script>');
-    console.log('My god',this.scriptNeeded, this.container);
-    //console.log('hhhh',this.container);
-    //const fragment = document.createRange().createContextualFragment(this.scriptNeeded);
-    // console.log('hhhh',this.container);
-    // this.container.appendChild(fragment);
+  constructor(private admissionsService:AdmissionsService) {
+
   }
  
   ngOnInit(): void {
@@ -32,21 +24,26 @@ export class AdmissionsComponent implements OnInit{
   	this.whyPeaceFlowerDisplayed = whyPeaceFlower;
   }
 
-  ngAfterViewInit(): void {
-    console.log('hhhh',this.container);
-    const fragment = document.createRange().createContextualFragment(this.scriptNeeded["changingThisBreaksApplicationSecurity"]);
-    console.log('fragment',fragment);
-    this.container.nativeElement.appendChild(fragment);
-  }
-
-  onSubmit(data): void {
-    this.admissionsService.submitToGoogleSheets(data).subscribe(data => {
-    	console.log('data',data);
-    },
-    err => {
-      console.log('error',err);
-    });
+  onSubmit(form): void {
+    let data = form.value;
+    let validity = form.valid;
+    if( validity === true) {
+      this.admissionsService.submitToGoogleSheets(data).subscribe(data => {
+        this.message = "Thank you for reaching out. We will get back to you soon.";
+        this.errorMessage = undefined;
+        form.reset();
+      },
+      err => {
+        this.errorMessage = "Sorry! An error occurred. Please try to reach out to us via email or phone.";
+        this.message = undefined;
+      });
+    } else {
+      this.errorMessage = "Please fill in the required fields";
+      this.message = undefined;
+    }
+    
     
   }
+
 }
 
--- src/app/services/admissions.service.ts
+++ src/app/services/admissions.service.ts
@@ -7,8 +7,14 @@ export class AdmissionsService {
   constructor(private http: HttpClient) {}
 
   submitToGoogleSheets(data): Observable<any> {
-  	console.log('data',data);
-  	return this.http.post('https://script.google.com/macros/s/AKfycbzMGgPjWOQTstsqo3GAHPM130yYV4heoF0E17cUfn9JIrAxkRc/exec', data, {
+  	// console.log('data',data);
+
+  	let encoded = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&');
+    
+    // console.log('encoded',encoded);
+  	return this.http.post('https://script.google.com/macros/s/AKfycbzMGgPjWOQTstsqo3GAHPM130yYV4heoF0E17cUfn9JIrAxkRc/exec', encoded, {
     headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded'),
   })
   }
